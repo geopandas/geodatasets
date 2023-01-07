07be9920417f0b2206a0d97d24f533eae369bd1b
Martin Fleischmann <martin@martinfleischmann.net>
2023-01-07 22:15:16 +0100

    ENH: add fetch() (#2)

--- xyzdata/__init__.py
+++ xyzdata/__init__.py
@@ -1,3 +1,3 @@
-from .api import get_path, get_url  # noqa
+from .api import get_path, get_url, fetch  # noqa
 from .data import data  # noqa
 from .lib import Bunch, DataItem  # noqa
--- xyzdata/tests/test_api.py
+++ xyzdata/tests/test_api.py
@@ -2,7 +2,7 @@ import os
 from pathlib import Path
 
 import pooch
-
+import pytest
 import xyzdata
 
 
@@ -14,12 +14,35 @@ def test_get_url():
     )
 
 
+@pytest.mark.request
 def test_get_path():
     in_cache = pooch.os_cache("xyzdata").joinpath("nybb_22c.zip")
     if Path(in_cache).exists():
         os.remove(in_cache)
 
-    path = Path(xyzdata.get_path("nybb"))
-    assert path.exists()
+    assert Path(xyzdata.get_path("nybb")).exists()
 
+    # cleanup
     os.remove(in_cache)
+
+
+@pytest.mark.request
+def test_fetch():
+    # clear cache
+    for data in ["airbnb.zip", "nybb_22c.zip", "boston.zip"]:
+        in_cache = pooch.os_cache("xyzdata").joinpath(data)
+        if Path(in_cache).exists():
+            os.remove(in_cache)
+
+    xyzdata.fetch("nybb")
+    assert pooch.os_cache("xyzdata").joinpath("nybb_22c.zip").exists()
+
+    xyzdata.fetch(["geoda airbnb", "geoda bostonhsg"])
+
+    for data in ["airbnb.zip", "boston.zip"]:
+        assert pooch.os_cache("xyzdata").joinpath("nybb_22c.zip").exists()
+
+    # cleanup
+    for data in ["airbnb.zip", "nybb_22c.zip", "boston.zip"]:
+        in_cache = pooch.os_cache("xyzdata").joinpath(data)
+        os.remove(in_cache)
