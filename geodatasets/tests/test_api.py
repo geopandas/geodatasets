e5ddb1b43d6052715f9dfe49f1f1094e1d182091
Martin Fleischmann <martin@martinfleischmann.net>
2023-03-06 11:16:11 +0100

    update tests using nybb

--- geodatasets/tests/test_api.py
+++ geodatasets/tests/test_api.py
@@ -10,13 +10,13 @@ def test_get_url():
     url = geodatasets.get_url("nybb")
     assert (
         url
-        == "https://data.cityofnewyork.us/api/geospatial/tqmj-j8zm?method=export&format=Original"  # noqa
+        == "https://www.nyc.gov/assets/planning/download/zip/data-maps/open-data/nybb_16a.zip"  # noqa
     )
 
 
 @pytest.mark.request
 def test_get_path():
-    in_cache = pooch.os_cache("geodatasets").joinpath("nybb_22c.zip")
+    in_cache = pooch.os_cache("geodatasets").joinpath("nybb_16a.zip")
     if Path(in_cache).exists():
         os.remove(in_cache)
 
@@ -29,13 +29,13 @@ def test_get_path():
 @pytest.mark.request
 def test_fetch():
     # clear cache
-    for data in ["airbnb.zip", "nybb_22c.zip", "nyc_neighborhoods.zip"]:
+    for data in ["airbnb.zip", "nybb_16a.zip", "nyc_neighborhoods.zip"]:
         in_cache = pooch.os_cache("geodatasets").joinpath(data)
         if Path(in_cache).exists():
             os.remove(in_cache)
 
     geodatasets.fetch("nybb")
-    assert pooch.os_cache("geodatasets").joinpath("nybb_22c.zip").exists()
+    assert pooch.os_cache("geodatasets").joinpath("nybb_16a.zip").exists()
 
     geodatasets.fetch(["geoda airbnb", "geoda atlanta"])
 
@@ -43,6 +43,6 @@ def test_fetch():
         assert pooch.os_cache("geodatasets").joinpath(data).exists()
 
     # cleanup
-    for data in ["airbnb.zip", "nybb_22c.zip", "atlanta_hom.zip"]:
+    for data in ["airbnb.zip", "nybb_16a.zip", "atlanta_hom.zip"]:
         in_cache = pooch.os_cache("geodatasets").joinpath(data)
         os.remove(in_cache)
