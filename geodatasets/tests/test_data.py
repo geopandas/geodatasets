5d3d5df49a2509da4d8bed4ad89cae9e5f0631c3
Martin Fleischmann <martin@martinfleischmann.net>
2023-09-12 10:21:37 +0200

    TST: test geometry_type

--- geodatasets/tests/test_data.py
+++ geodatasets/tests/test_data.py
@@ -12,3 +12,11 @@ def test_data_exists(name):
     gdf = gpd.read_file(geodatasets.get_path(name), engine="pyogrio")
     assert isinstance(gdf, pd.DataFrame)
     assert gdf.shape == (dataset.nrows, dataset.ncols)
+    if not (
+        ".csv" in dataset.filename
+        or (
+            hasattr(dataset, "members")
+            and any(".csv" in member for member in dataset.members)
+        )
+    ):
+        assert gdf.geom_type.str.endswith(dataset.geometry_type).all()
